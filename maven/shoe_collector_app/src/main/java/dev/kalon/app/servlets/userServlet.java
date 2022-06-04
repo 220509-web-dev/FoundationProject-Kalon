ef423bb307de5bd79027e304dd0923251ec817b3
Kalon Penagraph <kalon345@ravature.net>
2022-06-03 21:38:29 -0500

    -.-

--- maven/shoe_collector_app/src/main/java/dev/kalon/app/servlets/userServlet.java
+++ maven/shoe_collector_app/src/main/java/dev/kalon/app/servlets/userServlet.java
@@ -3,7 +3,6 @@ package dev.kalon.app.servlets;
 import dev.kalon.daos.appUserDAO;
 import dev.kalon.daos.appUserDAOPostgres;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
