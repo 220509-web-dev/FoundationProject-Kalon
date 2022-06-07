5218cecda4ab4e4d7e75dd4b410a904bebb1e1f0
Kalon Penagraph <kalon345@ravature.net>
2022-06-07 17:17:30 -0500

    added AuthService and UserService

---
+++ maven/shoe_collector_app/src/main/java/dev/kalon/app/services/AuthService.java
@@ -0,0 +1,52 @@
+package dev.kalon.app.services;
+
+import dev.kalon.app.daos.appUserDAO;
+import dev.kalon.app.entities.User;
+import dev.kalon.app.utils.exceptions.InvalidCredentialsException;
+import dev.kalon.app.utils.exceptions.UsernameNotAvailableException;
+
+public class AuthService {
+
+    private final appUserDAO userDAO;
+
+    public AuthService(appUserDAO userDAO){
+        this.userDAO = userDAO;
+    }
+
+    public static void register(User userToBeRegistered) {
+
+        if (userService.getUserByUsername(userToBeRegistered.getUsername()) != null) {
+
+            throw new UsernameNotAvailableException("Username is already taken.");
+        }
+
+        userService.insert(userToBeRegistered);
+    }
+
+    UserService userService = new UserService();
+
+    public User login(String username, String password) {
+
+        User user;
+
+        user = userService.getUserByUsername(username);
+
+        if (user != null && password.equals(user.getPassword())) {
+
+            System.out.println("Logged in successfully!");
+            return user;
+
+        } else if (user != null && !password.equals(user.getPassword())) {
+
+            System.out.println("Wrong Password");
+            throw new InvalidCredentialsException("Wrong password entered.");
+
+        } else {
+
+            System.out.println("User Does not exist!");
+            throw new InvalidCredentialsException("User does not exist.");
+        }
+    }
+}
+
+
---
+++ maven/shoe_collector_app/src/main/java/dev/kalon/app/services/UserService.java
@@ -0,0 +1,8 @@
+package dev.kalon.app.services;
+
+import dev.kalon.app.entities.User;
+
+public class UserService {
+    public User getUserByUsername(String username) {
+    }
+}
